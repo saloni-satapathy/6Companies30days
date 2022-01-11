2a18336cc375dcbb11e0b36883e8ca755ea4278e
saloni-satapathy <saloni.sushree@gmail.com>
2022-01-12 01:11:05 +0530

    Add files via upload

---
+++ array pair sum divisibility.cpp.cpp
@@ -0,0 +1,57 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+  public:
+    bool canPair(vector<int> nums, int k) {
+        // Code here.
+         if(nums.size()%2!=0)
+          return false;
+        map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]%k]+=1;
+        }
+        for(int i=0;i<nums.size();i++){
+            int rem=nums[i]%k;
+            if(rem==0){
+                if(mp[rem]%2!=0){
+                    return false;
+                }
+                
+            }else if(2*rem == k){
+                if(mp[rem]%2!=0){
+                    return false;
+                }
+                
+            }else {
+                if(mp[rem] != mp[(k-rem)]){
+                    return false;
+                }
+                
+            }
+        }
+        
+        return true;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n, k;
+        cin >> n >> k;
+        vector<int> nums(n);
+        for (int i = 0; i < nums.size(); i++) cin >> nums[i];
+        Solution ob;
+        bool ans = ob.canPair(nums, k);
+        if (ans)
+            cout << "True\n";
+        else
+            cout << "False\n";
+    }
+    return 0;
+}  // } Driver Code Ends
