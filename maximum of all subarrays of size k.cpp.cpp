0f96b21da2756aac1929dec0cc789e19f50aba1b
saloni-satapathy <saloni.sushree@gmail.com>
2022-01-17 00:28:06 +0530

    Add files via upload

---
+++ maximum of all subarrays of size k.cpp.cpp
@@ -0,0 +1,70 @@
+/ { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution
+{
+  public:
+    //Function to find maximum of each subarray of size k.
+    vector <int> max_of_subarrays(int *arr, int n, int k)
+    {
+        // your code here
+        deque<int>dq;
+        vector<int>ans;
+        dq.push_back(0);
+        
+        //dq.push_back(0);
+        for(int i=0;i<k;i++){
+           while(!dq.empty() && arr[i]>=arr[dq.back()]){
+                dq.pop_back();
+            } 
+            dq.push_back(i);
+        }
+        ans.push_back(arr[dq.front()]);
+        
+        for(int i=k;i<n;i++){
+            if(!dq.empty() && i-k>=dq.front()){
+                dq.pop_front();
+            }
+            while(!dq.empty() && arr[i]>=arr[dq.back()]){
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            ans.push_back(arr[dq.front()]);
+        }
+        return ans;
+    
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin >> t;
+	
+	while(t--){
+	    
+	    int n, k;
+	    cin >> n >> k;
+	    
+	    int arr[n];
+	    for(int i = 0;i<n;i++) 
+	        cin >> arr[i];
+	    Solution ob;
+	    vector <int> res = ob.max_of_subarrays(arr, n, k);
+	    for (int i = 0; i < res.size (); i++) 
+	        cout << res[i] << " ";
+	    cout << endl;
+	    
+	}
+	
+	return 0;
+}  // } Driver Code Ends
