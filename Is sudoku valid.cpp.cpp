bbef121754bbbe0d95349654a770cae8f666e9ca
saloni-satapathy <saloni.sushree@gmail.com>
2022-01-18 23:46:49 +0530

    Add files via upload

---
+++ Is sudoku valid.cpp.cpp
@@ -0,0 +1,57 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int isValid(vector<vector<int>> mat){
+        // code here
+        unordered_set<string>visited;
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(mat[i][j] != 0){
+                if( visited.find("row"+to_string(i)+to_string(mat[i][j]))==visited.end() ){
+                    visited.insert("row"+to_string(i)+to_string(mat[i][j]));
+                }else{
+                    return 0;
+                }
+                if(visited.find("column"+to_string(j)+to_string(mat[i][j]))==visited.end() ){
+                    visited.insert("column"+to_string(j)+to_string(mat[i][j]));
+                }else{
+                    return 0;
+                }
+                if(visited.find("subbox"+to_string((i/3)*3+(j/3))+to_string(mat[i][j]))==visited.end()){
+                    visited.insert("subbox"+to_string((i/3)*3+(j/3))+to_string(mat[i][j]));
+                }else{
+                    return 0;
+                }
+                
+                }
+                
+            }
+        }
+        
+        return 1;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        vector<vector<int>> mat(9, vector<int>(9, 0));
+        for(int i = 0;i < 81;i++)
+            cin>>mat[i/9][i%9];
+        
+        Solution ob;
+        cout<<ob.isValid(mat)<<"\n";
+    }
+    return 0;
+}  // } Driver Code Ends
