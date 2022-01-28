31d0d8b543668227a146cd4f37750e7bd10e4538
saloni-satapathy <saloni.sushree@gmail.com>
2022-01-28 10:26:53 +0530

    Add files via upload

---
+++ Minimum No of Steps to reach the destination.cpp.cpp
@@ -0,0 +1,36 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int minSteps(int D){
+        // code here
+        int step=0,sum=0;
+         while(sum<D|| (sum-D)%2!=0){
+             step++;
+              sum +=step;
+         }
+          return step;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int D;
+        cin>>D;
+        
+        Solution ob;
+        cout<<ob.minSteps(D)<<"\n";
+    }
+    return 0;
+}  // } Driver Code Ends
